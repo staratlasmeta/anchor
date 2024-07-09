9b00c5cf3ab838859bb396d93a9a53074a7ff6b8
Brett Etter <brett.etter@staratlas.com>
2024-07-09 13:48:10 -0500

    Patched allocator

--- lang/src/unreal.rs
+++ lang/src/unreal.rs
@@ -2,29 +2,59 @@ use solana_program::instruction::{AccountMeta, Instruction};
 use solana_program::pubkey::Pubkey;
 use std::alloc::{GlobalAlloc, Layout};
 use std::ffi::c_void;
-use std::ptr::slice_from_raw_parts_mut;
+use std::ptr::{null_mut, slice_from_raw_parts_mut, NonNull};
+use std::sync::atomic::{AtomicPtr, Ordering};
 
-extern "C" {
-    // FMemory::Malloc
-    fn rust_malloc(size: usize, alignment: usize) -> *mut c_void;
-    // FMemory::Free
-    fn rust_free(ptr: *mut c_void);
+// extern "C" {
+//     // FMemory::Malloc
+//     fn rust_malloc(size: usize, alignment: usize) -> *mut c_void;
+//     // FMemory::Free
+//     fn rust_free(ptr: *mut c_void);
+// }
+
+struct AllocatorFunctions {
+    malloc: extern "C" fn(usize, usize) -> *mut c_void,
+    free: extern "C" fn(*mut c_void),
 }
-
-struct Alloc;
+struct Alloc(AtomicPtr<AllocatorFunctions>);
 
 unsafe impl GlobalAlloc for Alloc {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        unsafe { rust_malloc(layout.size(), layout.align()) as *mut u8 }
+        match NonNull::new(self.0.load(Ordering::SeqCst)) {
+            None => panic!("Allocator not set"),
+            Some(f) => unsafe { ((*f.as_ptr()).malloc)(layout.size(), layout.align()) as *mut u8 },
+        }
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        unsafe { rust_free(ptr as *mut c_void) }
+        match NonNull::new(self.0.load(Ordering::SeqCst)) {
+            None => panic!("Allocator not set"),
+            Some(f) => unsafe { ((*f.as_ptr()).free)(ptr as *mut c_void) },
+        }
     }
 }
 
 #[global_allocator]
-static ALLOC: Alloc = Alloc;
+static ALLOC: Alloc = Alloc(AtomicPtr::new(null_mut()));
+
+#[no_mangle]
+extern "C" fn set_allocator_functions(
+    malloc: extern "C" fn(usize, usize) -> *mut c_void,
+    free: extern "C" fn(*mut c_void),
+) {
+    if ALLOC
+        .0
+        .compare_exchange(
+            null_mut(),
+            Box::leak(Box::new(AllocatorFunctions { malloc, free })),
+            Ordering::SeqCst,
+            Ordering::SeqCst,
+        )
+        .is_err()
+    {
+        panic!("Allocator already set");
+    }
+}
 
 #[repr(C)]
 pub struct BetterCInstruction {
